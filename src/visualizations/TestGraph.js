18d3401c302aaddc12b307a8bfe7ba14f951a534
vishruty <vishruty@gmail.com>
2021-12-15 13:07:59 +0100

    Edited temperature graph

--- src/visualizations/TestGraph.js
+++ src/visualizations/TestGraph.js
@@ -82,86 +82,87 @@ function(data) {
 //Thanks to https://gist.github.com/d3noob/2a27cdf2574e7f5e31a4844057d6b824 for idea on how to loop the animation
 function lineTransition(){
 
-let path = svg
-  .append("path")
-  .datum(data)
-  .attr("fill", "none")
-  .attr("stroke", "#D99056")
-  .attr("stroke-width", 3)
-  .attr("d", d3.line()
-    .x(d => x(d.Day))
-    .y(d => y(d.temperature_anomaly))
-    // .x(function(d) { return x(d.x) })
-    // .y(function(d) { return y(d.y) })
-  )
-var totalLength = path.node().getTotalLength();
-repeat();
-
-function repeat(){
-path
-  .attr("stroke-dasharray", totalLength + " " + totalLength)
-  .attr("stroke-dashoffset", totalLength)
-  .transition()
-  .duration(4000)
-  .ease(d3.easeLinear)
-  .attr("stroke-dashoffset", 0)
-  .on("end", repeat);
-
+  let path = svg
+    .append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "#D99056")
+    .attr("stroke-width", 3)
+    .attr("d", d3.line()
+      .x(d => x(d.Day))
+      .y(d => y(d.temperature_anomaly))
+      // .x(function(d) { return x(d.x) })
+      // .y(function(d) { return y(d.y) })
+    )
+  var totalLength = path.node().getTotalLength();
+  repeat();
+  
+  function repeat(){
+  path
+    .attr("stroke-dasharray", totalLength + " " + totalLength)
+    .attr("stroke-dashoffset", totalLength)
+    .transition()
+    .duration(4000)
+    .ease(d3.easeLinear)
+    .attr("stroke-dashoffset", 0)
+    .transition()
+    .duration(1000)
+    .on("end", repeat);
+  
+    };
+  
   };
-
-};
-
-
-  // create a tooltip
-  const Tooltip = d3.select("#my_dataviz")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
-    .style("position", "absolute")
-    .style("color", "black")
-
-
-    // Three function that change the tooltip when user hover / move / leave a cell
-    const mouseover = function(event,d) {
-      Tooltip
-        .style("opacity", 1)
-    }
-    const mousemove = function(event,d) {
-      Tooltip
-        .html("Year: "+ d.Day.getFullYear() + "<br>Temperature increase: " + (Math.round(d.temperature_anomaly*100))/100 +"°C")
-        .style("left", (event.pageX) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-        .style("top", (event.pageY) + "px");
-    }
-    const mouseleave = function(event,d) {
-      Tooltip
-        .style("opacity", 0)
-    }
-
-    lineTransition()
-
-  // Add the points
-  svg
-    .append("g")
-    .selectAll("dot")
-    .data(data)
-    .join("circle")
-      .attr("class", "myCircle")
-      .attr("cx", d => x(d.Day))
-      .attr("cy", d => y(d.temperature_anomaly))
-      .attr("r", 2.5)
-      .attr("stroke", "white")
-      .attr("stroke-width", 2)
-    //  
-      .attr("fill", "#80847E")
-      .on("mouseover", mouseover)
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave)
-
+  
+  
+    // create a tooltip
+    const Tooltip = d3.select("#my_dataviz")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px")
+      .style("position", "absolute")
+      .style("color", "black")
+  
+  
+      // Three function that change the tooltip when user hover / move / leave a cell
+      const mouseover = function(event,d) {
+        Tooltip
+          .style("opacity", 1)
+      }
+      const mousemove = function(event,d) {
+        Tooltip
+          .html("Year: "+ d.Day.getFullYear() + "<br>Temperature increase: " + (Math.round(d.temperature_anomaly*100))/100 +"°C")
+          .style("left", (event.pageX) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+          .style("top", (event.pageY) + "px");
+      }
+      const mouseleave = function(event,d) {
+        Tooltip
+          .style("opacity", 0)
+      }
+  
+      lineTransition()
+  
+    // Add the points
+    svg
+      .append("g")
+      .selectAll("dot")
+      .data(data)
+      .join("circle")
+        .attr("class", "myCircle")
+        .attr("cx", d => x(d.Day))
+        .attr("cy", d => y(d.temperature_anomaly))
+        .attr("r", 2.5)
+        .attr("stroke", "#80847E")
+        .attr("stroke-width", 2)
+      //  
+        .attr("fill", "#80847E")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
 
 
                     
